9f79af756662e73a0530ddb1ec57f2d46a6aa763
Ishmet <ishmetjahan@yahoo.com>
2019-07-01 01:38:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,39 @@
-require 'pry'
+#require 'pry'
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false 
+    end
   end
 
   def question?
-
+  if self.end_with?("?")
+    true
+  else
+    false
+  end
   end
 
   def exclamation?
-
+  if self.end_with?("!")
+    true
+  else
+    false
+  end
   end
 
   def count_sentences
-
+   #binding.pry
+   a= self.split(".").length
+  # b= self.split("!").length
+  # c= self.split("?").length
+  # final = a+b+c
+  # final
+  #self.split("/\[.,?,!]/").length
+  
   end
 end
